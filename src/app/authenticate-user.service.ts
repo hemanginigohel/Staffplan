fc72e57dfb92ea361137e0ab28bd8eb52090371a
hemangini <hemangini.gohel@bacancytechnology.com>
2019-02-24 15:36:20 +0530

    done

--- src/app/authenticate-user.service.ts
+++ src/app/authenticate-user.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { HttpHeaders } from '@angular/common/http';
 import { CookieService } from "ngx-cookie-service";
 import { Observable } from 'rxjs';
-
+import { map } from "rxjs/operators";
 @Injectable({
   providedIn: 'root'
 })
